e6f8fac4faf37e22612150369b6768ea2da392ce
yair dror <yairx66@gmail.com>
2025-04-21 18:02:17 +0300

    first commit

---
+++ project.js
@@ -0,0 +1,54 @@
+function newNote() {
+    const newTitle = document.querySelector('#addTitle').value
+    const newDate = document.querySelector('#addDate').value
+    const newTime = document.querySelector('#addTime').value
+    const newTask = document.querySelector('#addTask').value
+
+    const note = {
+        id: Date.now(),
+        title: newTitle,
+        date: newDate,
+        time: newTime,
+        task: newTask
+    }
+
+    const notes = JSON.parse(localStorage.getItem('notes')) || []
+    notes.unshift(note)
+    localStorage.setItem('notes', JSON.stringify(notes))
+
+    // document.querySelector('#myForm').reset()
+    savedNotes()
+}
+
+function savedNotes() {
+    const notes = JSON.parse(localStorage.getItem('notes')) || []
+    let allCardsHtml = ''
+
+    for (let i = 0; i < notes.length; i++) {
+        const note = notes[i]
+        allCardsHtml += `
+            <div class="card" style="width: 18rem;" data-id="${note.id}">
+                <div class="card-body">
+                    <button class="close" onclick="deleteNote(${note.id})">❌</button>
+                    <h2 class="card-title">${note.title}</h2>
+                    <h5 class="card-text">${note.task}</h5>
+                    <div class="time">
+                        <p>תאריך: ${note.date}</p>
+                        <p>שעה: ${note.time}</p>
+                    </div>
+                </div>
+            </div>`
+    }
+
+    document.querySelector('#notes').innerHTML = allCardsHtml
+}
+
+
+function deleteNote(id) {
+    let notes = JSON.parse(localStorage.getItem('notes')) || []
+    notes = notes.filter(note => note.id !== id)
+    localStorage.setItem('notes', JSON.stringify(notes))
+    savedNotes()
+}
+
+savedNotes()
